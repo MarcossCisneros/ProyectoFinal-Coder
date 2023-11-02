37ff384d4303a3d584f7d41ac1a79ed6ff90e378
Marcos <marcoscisneros674@gmail.com>
2023-11-02 11:21:31 -0300

    ultimas modificaciones al proyecto

--- src/components/header/Carrito.jsx
+++ src/components/header/Carrito.jsx
@@ -14,23 +14,23 @@ const Carrito = () => {
       <h1 className="main-title">Carrito</h1>
 
       {carrito.map((prod) => (
-        <div key={prod.id}>
-          <br />
+        <div key={prod.id} className="carrito__container">
           <h3>{prod.title}</h3>
           <img src={prod.img} alt={prod.title} />
-          <p>Precio unit: ${prod.price}</p>
-          <p>Precio total: ${Number(prod.price * prod.cantidad)}</p>
-          <p>Cant: {prod.cantidad}</p>
-          <br />
+          <div className="carrito__container-items">
+            <p>Precio unit: ${prod.price}</p>
+            <p>Precio total: ${Number(prod.price * prod.cantidad)}</p>
+            <p>Cant: {prod.cantidad}</p>
+          </div>
         </div>
       ))}
 
       {carrito.length > 0 ? (
-        <>
+        <div className="carrito__detail">
           <h2>Precio total: ${precioTotal()}</h2>
           <button onClick={handleVaciar}>Vaciar</button>
           <Link to="/checkout">Finalizar compra</Link>
-        </>
+        </div>
       ) : (
         <h2>El carrito está vacío :(</h2>
       )}
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
+import menuClose from "../../assets/close-menu.png";
+import menuOpen from "../../assets/open-menu.png";
 
 const Navbar = () => {
   function abrirCerrar() {
@@ -14,32 +16,11 @@ const Navbar = () => {
         <h1>DriFF</h1>
       </Link>
       <button className="buttonMenu" onClick={abrirCerrar}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="currentColor"
-          class="bi bi-list"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-          />
-        </svg>
+        <img src={menuOpen} alt="openMenu" />
       </button>
       <ul className="menu not-visible">
         <button className="buttonMenu" onClick={abrirCerrar}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            class="bi bi-x-lg"
-            viewBox="0 0 16 16"
-          >
-            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-          </svg>
+          <img src={menuClose} alt="close" />
         </button>
         <li>
           <Link className="menu-link" to="/">
